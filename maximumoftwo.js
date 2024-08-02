3f9aa7016e1a8794ad06569fe0e536560e57dfa9
P-UJWALA <164027329+P-UJWALA@users.noreply.github.com>
2024-08-02 19:39:40 +0530

    Add files via upload

---
+++ maximumoftwo.js
@@ -0,0 +1,10 @@
+console.log("helloworld");
+// write a javascript program to find the maximum between to numbers
+let a=100;
+let b=20;
+if(a>b){
+    console.log("the maximum number is"+a)
+}
+else{
+    console.log("the maximum number is"+b)
+}
