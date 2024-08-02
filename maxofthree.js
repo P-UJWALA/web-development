54e809566c4d6c172bb9b946ecb5f81e4a9bbde5
P-UJWALA <164027329+P-UJWALA@users.noreply.github.com>
2024-08-02 19:38:21 +0530

    Add files via upload

---
+++ maxofthree.js
@@ -0,0 +1,5 @@
+let a=45
+let b=56
+let c=67
+(a>b && a>c) ?console.log("max number is"+a):console.log("max number is not"+a)
+(b>a && b>c)?console.log("max number is"+b):console.log("max number is"+c)
